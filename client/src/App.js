17caa3fb164d8e6f811efa9076f95902d0203ea7
Racksoup <rack.connor1@gmail.com>
2023-03-29 12:58:09 -0400

    init

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+import Home from './UI/Pages/Home/Home.jsx';
+// import Navbar from './UI/Components/Navbar/Navbar.jsx';
+
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className='App'>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        {/* <Route path='/portfolio' element={<Portfolio />} /> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const path = require('path');
+const axios = require('axios');
+
+dotenv.config();
+
+const app = express();
+
+// connectDB();
+
+app.use(express.json({ extend: false }));
+
+// routes
+// app.use('/api/youtubev3', require('./routes/api/youtubev3'));
+
+// production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
